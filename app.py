8bfb5c55a9dcc2e92a11156a19bf5247279d1695
Mikhail Yudin <fagci.nsk@gmail.com>
2020-10-29 00:01:06 +0700

    rm pandas

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import plotly
 import psutil
 import eventlet
 import numpy as np
-import pandas as pd
-from plotly.graph_objs import Scatter, Layout
+from plotly.graph_objs import Scattergl, Layout
 from collections import deque
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
@@ -14,16 +13,16 @@ socketio = SocketIO(app)
 
 N = 60
 x = np.linspace(0, N, N)
-y_mem = deque([0 for _ in range(0, N)], N)
-y_cpu = deque([0 for _ in range(0, N)], N)
+y_mem = deque([None for _ in range(0, N)], N)
+y_cpu = deque([None for _ in range(0, N)], N)
 
 cpu_count = psutil.cpu_count()
 mem_total = psutil.virtual_memory().total
 
 def update_plot():
     while True:
-        json, data = create_plot()
-        socketio.emit('plot', data)
+        plot = create_plot()
+        socketio.emit('plot', plot)
         eventlet.sleep(1)
 
 
@@ -34,19 +33,15 @@ def create_plot():
     y_mem.append(mem * 100 / mem_total)
     y_cpu.append(cpu)
 
-    df1 = pd.DataFrame({'x': x, 'y': y_mem})
-    df2 = pd.DataFrame({'x': x, 'y': y_cpu})
-
     data = [
-            Scatter(x=df1['x'], y=df1['y'], name='RAM'),
-            Scatter(x=df2['x'], y=df2['y'], name='CPU')
+            Scattergl(x=x, y=list(y_mem), name='RAM'),
+            Scattergl(x=x, y=list(y_cpu), name='CPU')
             ]
 
     layout = Layout(yaxis=dict(range=[0, 100]))
 
-    graphJSON = json.dumps(dict(data=data,layout=layout), cls=plotly.utils.PlotlyJSONEncoder)
-
-    return (graphJSON, json.loads(graphJSON))
+    enc = plotly.utils.PlotlyJSONEncoder
+    return json.loads(json.dumps(dict(data=data, layout=layout), cls=enc))
 
 
 @app.route('/')
